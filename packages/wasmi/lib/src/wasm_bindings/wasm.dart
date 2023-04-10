3a4bf15595669b2e7149ac22da30fcc5ea50dd99
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-04-09 20:59:44 -0500

    add stub for wasm interface

--- packages/wasmi/lib/src/wasm_bindings/wasm.dart
+++ packages/wasmi/lib/src/wasm_bindings/wasm.dart
@@ -1,13 +1,16 @@
 import 'dart:typed_data';
 
-import '_wasm_interop_native.dart'
+import '_wasm_interop_stub.dart'
+    if (dart.library.io) '_wasm_interop_native.dart'
     if (dart.library.html) '_wasm_interop_web.dart' as platform_impl;
 import 'wasm_interface.dart';
 
-export 'wasm_interface.dart';
-
 Future<WasmModule> compileAsyncWasmModule(Uint8List bytes) async {
   return platform_impl.compileAsyncWasmModule(bytes);
+}
+
+WasmModule compileWasmModule(Uint8List bytes) {
+  return platform_impl.compileWasmModule(bytes);
 }
 
 class WasmInstanceModule {
