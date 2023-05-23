45106e9587e4ac03b929136153db38f134f53158
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-05-23 00:06:32 -0500

    use path dependencies for examples

--- packages/dart_wit_component/wasm_wit_component/bin/generate.dart
+++ packages/dart_wit_component/wasm_wit_component/bin/generate.dart
@@ -6,13 +6,27 @@ import 'package:wasm_wit_component/wasm_wit_component.dart';
 
 void main(List<String> arguments) async {
   final witFilePath = arguments[1];
-  final witFile = File(witFilePath);
-  if (witFile.existsSync() == false) {
+  final type = FileSystemEntity.typeSync(witFilePath);
+  if (type == FileSystemEntityType.notFound) {
     throw Exception('wit file not found: $witFilePath');
   }
 
-  // TODO: multiple files
+  // TODO: multiple files or directory
   final dartFilePath = arguments.length > 2 ? arguments[2] : null;
+
+  final allowedDir = type == FileSystemEntityType.file
+      ? File(witFilePath).parent
+      : Directory(witFilePath);
+  final inputs = type == FileSystemEntityType.file
+      ? WitGeneratorInput.listWitFile([
+          WitFile(
+            path: witFilePath,
+            content: await File(witFilePath).readAsString(),
+          ),
+        ])
+      : WitGeneratorInput.witGeneratorPaths(
+          WitGeneratorPaths(inputPath: witFilePath),
+        );
 
   final wasmFile = await File('lib/dart_wit_component.wasm').readAsBytes();
   final module = await compileWasmModule(wasmFile);
@@ -28,8 +42,8 @@ void main(List<String> arguments) async {
       preopenedFiles: const [],
       preopenedDirs: [
         PreopenedDir(
-          hostPath: witFile.parent.path,
-          wasmGuestPath: witFile.parent.path,
+          hostPath: allowedDir.path,
+          wasmGuestPath: allowedDir.path,
         ),
       ],
       browserFileSystem: const {},
@@ -42,12 +56,7 @@ void main(List<String> arguments) async {
 
   final result = world.generate(
     config: WitGeneratorConfig(
-      inputs: WitGeneratorInput.listWitFile([
-        WitFile(
-          path: witFilePath,
-          content: await witFile.readAsString(),
-        ),
-      ]),
+      inputs: inputs,
       jsonSerialization: true,
       copyWith_: true,
       equalityAndHashCode: true,
@@ -58,8 +67,8 @@ void main(List<String> arguments) async {
   switch (result) {
     case Ok(:final ok):
       for (final file in ok) {
-        final outPath =
-            dartFilePath ?? file.path.replaceFirst(RegExp(r'.wit$'), '.dart');
+        final outPath = dartFilePath ??
+            file.path.replaceFirst(RegExp(r'(.wit)?$'), '.dart');
         await File(outPath).writeAsString(file.content);
       }
     case Err(:final error):
