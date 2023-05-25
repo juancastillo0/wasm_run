3e2714e8cddedfdcb82c31b2a2e2e28a3eaf7cff
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-05-24 23:57:52 -0500

    use WasmRunLibrary.setUp for WasmRunFlutterWeb.registerWith

--- packages/wasm_run_flutter/lib/wasm_run_flutter_web.dart
+++ packages/wasm_run_flutter/lib/wasm_run_flutter_web.dart
@@ -1,29 +1,8 @@
-import 'dart:html' as html;
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:wasm_run/wasm_run.dart';
 
 class WasmRunFlutterWeb {
   static void registerWith(Registrar registrar) {
-    _injectSrcScript('./packages/wasm_run/assets/wasm-feature-detect.js');
-    _injectSrcScript(
-      './packages/wasm_run/assets/browser_wasi_shim.js',
-      type: 'module',
-    );
-  }
-
-  /// Injects a `script` with a `src` dynamically into the head of the current
-  /// document.
-  static Future<void> _injectSrcScript(
-    String src, {
-    String type = 'application/javascript',
-  }) {
-    final script = html.ScriptElement();
-    script.type = type;
-    script.src = src;
-    script.defer = true;
-    // script.async = true;
-    assert(html.document.head != null);
-    html.document.head!.append(script);
-    return script.onLoad.first;
+    WasmRunLibrary.setUp(override: false);
   }
 }
