e2c56dd49a36410993de4c18732e6e9518be4426
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-05-30 08:14:44 -0500

    parse additional params in generator

--- packages/dart_wit_component/wasm_wit_component/lib/src/generate_cli.dart
+++ packages/dart_wit_component/wasm_wit_component/lib/src/generate_cli.dart
@@ -82,7 +82,7 @@ class GeneratorCLIArgs {
 
   /// Creates a new [GeneratorCLIArgs] from the given [arguments].
   factory GeneratorCLIArgs.fromArgs(List<String> arguments) {
-    final args = _CLIArgs(arguments);
+    final args = _CLIArgs(arguments: arguments, valueNames: _Arg.allValues);
     final positional = args.positional;
     if (positional.isEmpty) {
       throw Exception('Missing positional argument `witInputPath`.');
@@ -93,7 +93,7 @@ class GeneratorCLIArgs {
       );
     }
     for (final e in args.namedBool.keys) {
-      if (!_Arg.all.contains(e)) {
+      if (!_Arg.allBool.contains(e)) {
         throw Exception('Unknown argument "$e".');
       }
     }
@@ -102,6 +102,14 @@ class GeneratorCLIArgs {
     final dartFilePath = positional.length > 1 ? positional[1] : null;
     final watch = args.namedBool[_Arg.watch] ?? false;
     final enableDefault = args.namedBool[_Arg.default_] ?? true;
+    final int64TypeList =
+        args.namedValues[_Arg.int64Type] ?? [Int64TypeConfig.bigInt.name];
+    if (int64TypeList.length > 1) {
+      throw Exception(
+        'Too many values for argument `int64Type`. $int64TypeList',
+      );
+    }
+    final int64Type = int64TypeList.first;
 
     final config = WitGeneratorConfig(
       inputs: WitGeneratorInput.fileSystemPaths(
@@ -112,6 +120,16 @@ class GeneratorCLIArgs {
       copyWith_: args.namedBool[_Arg.copyWith] ?? enableDefault,
       equalityAndHashCode: args.namedBool[_Arg.equality] ?? enableDefault,
       toString_: args.namedBool[_Arg.toString_] ?? enableDefault,
+      generateDocs: args.namedBool[_Arg.generateDocs] ?? enableDefault,
+      fileHeader: const None(),
+      useNullForOption: false,
+      int64Type: Int64TypeConfig.values.firstWhere(
+        (e) => e.name == int64Type,
+        orElse: () => throw Exception(
+          'Unknown int64 type: $int64Type. Options: '
+          '${Int64TypeConfig.values.map((e) => e.name).join(', ')}',
+        ),
+      ),
     );
 
     return GeneratorCLIArgs(
@@ -146,15 +164,23 @@ class _Arg {
   static const copyWith = 'copy-with';
   static const equality = 'equality';
   static const toString_ = 'to-string';
+  static const generateDocs = 'generate-docs';
   static const watch = 'watch';
 
-  static const all = [
+  static const int64Type = 'int64-type';
+
+  static const allBool = [
     default_,
     jsonSerialization,
     copyWith,
     equality,
     toString_,
+    generateDocs,
     watch,
+  ];
+
+  static const allValues = [
+    int64Type,
   ];
 }
 
@@ -162,32 +188,62 @@ class _CLIArgs {
   final List<String> arguments;
   final List<String> positional;
   final Map<String, bool> namedBool;
+  final Map<String, List<String>> namedValues;
 
-  _CLIArgs(this.arguments)
-      : positional = arguments.where((e) => !e.startsWith('-')).toList(),
-        namedBool = {} {
-    for (final arg in arguments.indexed.where((e) => e.$2.startsWith('-'))) {
+  _CLIArgs({
+    required this.arguments,
+    required List<String>? valueNames,
+  })  : positional = [],
+        namedBool = {},
+        namedValues = {} {
+    final Set<int> usedIndices = {};
+    for (final arg in arguments.indexed) {
+      if (!arg.$2.startsWith('-')) {
+        // Positional
+        if (!usedIndices.contains(arg.$1)) {
+          positional.add(arg.$2);
+        }
+        continue;
+      }
       if (!arg.$2.startsWith('--')) {
         throw Exception('Invalid argument $arg. Should be --<name>');
       }
       final parts = arg.$2.substring(2).split('=');
       String name = parts[0];
-      final isNegated = name.startsWith('no-');
-      if (isNegated) {
-        name = name.substring(3);
-      }
-      if (namedBool.containsKey(name)) {
-        throw Exception('Duplicate argument $arg.');
-      }
-      if (parts.length == 1) {
-        namedBool[name] = !isNegated;
-      } else if (parts.length == 2) {
-        if (!const ['true', 'false'].contains(parts[1])) {
-          throw Exception('Invalid argument $arg. Should be true or false.');
+      if (valueNames != null && valueNames.contains(name)) {
+        // Named Values
+        final list = namedValues.putIfAbsent(name, () => []);
+        if (parts.length == 1) {
+          final next = arguments.elementAtOrNull(arg.$1 + 1);
+          if (next == null || next.startsWith('-')) {
+            throw Exception('Missing value for argument $arg.');
+          }
+          usedIndices.add(arg.$1 + 1);
+          list.add(next);
+        } else if (parts.length == 2) {
+          list.add(parts[1]);
+        } else {
+          throw Exception('Invalid value argument $arg.');
         }
-        namedBool[name] = parts[1] == 'true';
       } else {
-        throw Exception('Invalid argument $arg.');
+        // Bool
+        final isNegated = name.startsWith('no-');
+        if (isNegated) {
+          name = name.substring(3);
+        }
+        if (namedBool.containsKey(name)) {
+          throw Exception('Duplicate argument $arg.');
+        }
+        if (parts.length == 1) {
+          namedBool[name] = !isNegated;
+        } else if (parts.length == 2) {
+          if (!const ['true', 'false'].contains(parts[1])) {
+            throw Exception('Invalid argument $arg. Should be true or false.');
+          }
+          namedBool[name] = parts[1] == 'true';
+        } else {
+          throw Exception('Invalid argument $arg.');
+        }
       }
     }
   }
--- packages/dart_wit_component/wasm_wit_component/lib/src/generator.dart
+++ packages/dart_wit_component/wasm_wit_component/lib/src/generator.dart
@@ -56,6 +56,36 @@ class WitFile {
       [(label: 'path', t: StringType()), (label: 'contents', t: StringType())]);
 }
 
+/// Configures how 64 bit integers are represented in the generated Dart code.
+enum Int64TypeConfig {
+  /// Use the native JavaScript BigInt type and [int] for native platforms.
+  /// You will need to cast it with the [i64] utility functions.
+  nativeObject,
+
+  /// Use the [BigInt] type for signed and unsigned 64 bit integers.
+  bigInt,
+
+  /// Use the [BigInt] type only for unsigned 64 bit integers.
+  /// [int] is used for signed 64 bit integers.
+  bigIntUnsignedOnly,
+
+  /// Use the [int] type.
+  coreInt;
+
+  factory Int64TypeConfig.fromJson(Object? json_) {
+    final json = json_ is Map ? json_.keys.first : json_;
+    if (json is String) {
+      final index = _spec.labels.indexOf(json);
+      return index != -1 ? values[index] : values.byName(json);
+    }
+    return values[json! as int];
+  }
+  Object? toJson() => _spec.labels[index];
+  static const _spec = EnumType(
+      ['native-object', 'big-int', 'big-int-unsigned-only', 'core-int']);
+}
+
+/// Files paths and their contents.
 class InMemoryFiles {
   /// The file to use as the world file.
   final WitFile worldFile;
@@ -123,6 +153,7 @@ class InMemoryFiles {
   ]);
 }
 
+/// The paths that specity wit files in the file system.
 class FileSystemPaths {
   /// May be a file or a directory.
   /// When it is a directory, all files in the directory will be used as input.
@@ -177,9 +208,11 @@ sealed class WitGeneratorInput {
       json = (k is int ? k : int.parse(k! as String), json.values.first);
     }
     return switch (json) {
-      (0, final value) =>
+      (0, final value) ||
+      [0, final value] =>
         WitGeneratorInputFileSystemPaths(FileSystemPaths.fromJson(value)),
-      (1, final value) =>
+      (1, final value) ||
+      [1, final value] =>
         WitGeneratorInputInMemoryFiles(InMemoryFiles.fromJson(value)),
       _ => throw Exception('Invalid JSON $json_'),
     };
@@ -258,12 +291,30 @@ class WitGeneratorConfig {
   /// Whether to generate toString methods for the types in the world.
   final bool toString_;
 
+  /// Whether to generate documentation for methods within the types in the world.
+  /// For example, for the `toJson`, `copyWith` and `fromJson` methods.
+  final bool generateDocs;
+
+  /// Custom imports or comments to be added at the top of the generated file.
+  final Option<String> fileHeader;
+
+  /// Whether to use `null` for optional types when possible.
+  /// For example when there is a single unnested option type.
+  final bool useNullForOption;
+
+  /// The type to use for 64 bit integers.
+  final Int64TypeConfig int64Type;
+
   const WitGeneratorConfig({
     required this.inputs,
     required this.jsonSerialization,
     required this.copyWith_,
     required this.equalityAndHashCode,
     required this.toString_,
+    required this.generateDocs,
+    required this.fileHeader,
+    required this.useNullForOption,
+    required this.int64Type,
   });
 
   factory WitGeneratorConfig.fromJson(Object? json_) {
@@ -276,14 +327,22 @@ class WitGeneratorConfig {
         final jsonSerialization,
         final copyWith_,
         final equalityAndHashCode,
-        final toString_
+        final toString_,
+        final generateDocs,
+        final fileHeader,
+        final useNullForOption,
+        final int64Type
       ] ||
       (
         final inputs,
         final jsonSerialization,
         final copyWith_,
         final equalityAndHashCode,
-        final toString_
+        final toString_,
+        final generateDocs,
+        final fileHeader,
+        final useNullForOption,
+        final int64Type
       ) =>
         WitGeneratorConfig(
           inputs: WitGeneratorInput.fromJson(inputs),
@@ -291,6 +350,11 @@ class WitGeneratorConfig {
           copyWith_: copyWith_! as bool,
           equalityAndHashCode: equalityAndHashCode! as bool,
           toString_: toString_! as bool,
+          generateDocs: generateDocs! as bool,
+          fileHeader: Option.fromJson(fileHeader,
+              (some) => some is String ? some : (some! as ParsedString).value),
+          useNullForOption: useNullForOption! as bool,
+          int64Type: Int64TypeConfig.fromJson(int64Type),
         ),
       _ => throw Exception('Invalid JSON $json_')
     };
@@ -301,6 +365,10 @@ class WitGeneratorConfig {
         'copy-with': copyWith_,
         'equality-and-hash-code': equalityAndHashCode,
         'to-string': toString_,
+        'generate-docs': generateDocs,
+        'file-header': fileHeader.toJson((some) => some),
+        'use-null-for-option': useNullForOption,
+        'int64-type': int64Type.toJson(),
       };
   WitGeneratorConfig copyWith({
     WitGeneratorInput? inputs,
@@ -308,15 +376,32 @@ class WitGeneratorConfig {
     bool? copyWith_,
     bool? equalityAndHashCode,
     bool? toString_,
+    bool? generateDocs,
+    Option<String>? fileHeader,
+    bool? useNullForOption,
+    Int64TypeConfig? int64Type,
   }) =>
       WitGeneratorConfig(
           inputs: inputs ?? this.inputs,
           jsonSerialization: jsonSerialization ?? this.jsonSerialization,
           copyWith_: copyWith_ ?? this.copyWith_,
           equalityAndHashCode: equalityAndHashCode ?? this.equalityAndHashCode,
-          toString_: toString_ ?? this.toString_);
-  List<Object?> get props =>
-      [inputs, jsonSerialization, copyWith_, equalityAndHashCode, toString_];
+          toString_: toString_ ?? this.toString_,
+          generateDocs: generateDocs ?? this.generateDocs,
+          fileHeader: fileHeader ?? this.fileHeader,
+          useNullForOption: useNullForOption ?? this.useNullForOption,
+          int64Type: int64Type ?? this.int64Type);
+  List<Object?> get props => [
+        inputs,
+        jsonSerialization,
+        copyWith_,
+        equalityAndHashCode,
+        toString_,
+        generateDocs,
+        fileHeader,
+        useNullForOption,
+        int64Type
+      ];
   @override
   String toString() =>
       'WitGeneratorConfig${Map.fromIterables(_spec.fields.map((f) => f.label), props)}';
@@ -353,7 +438,15 @@ class WitGeneratorConfig {
     (label: 'json-serialization', t: Bool()),
     (label: 'copy-with', t: Bool()),
     (label: 'equality-and-hash-code', t: Bool()),
-    (label: 'to-string', t: Bool())
+    (label: 'to-string', t: Bool()),
+    (label: 'generate-docs', t: Bool()),
+    (label: 'file-header', t: OptionType(StringType())),
+    (label: 'use-null-for-option', t: Bool()),
+    (
+      label: 'int64-type',
+      t: EnumType(
+          ['native-object', 'big-int', 'big-int-unsigned-only', 'core-int'])
+    )
   ]);
 }
 
@@ -368,7 +461,7 @@ class DartWitGeneratorWorld {
   DartWitGeneratorWorld({
     required this.imports,
     required this.library,
-  }) : _generate = library.getComponentFunction(
+  })  : _generate = library.getComponentFunction(
           'generate',
           const FuncType([
             (
@@ -399,7 +492,19 @@ class DartWitGeneratorWorld {
                 (label: 'json-serialization', t: Bool()),
                 (label: 'copy-with', t: Bool()),
                 (label: 'equality-and-hash-code', t: Bool()),
-                (label: 'to-string', t: Bool())
+                (label: 'to-string', t: Bool()),
+                (label: 'generate-docs', t: Bool()),
+                (label: 'file-header', t: OptionType(StringType())),
+                (label: 'use-null-for-option', t: Bool()),
+                (
+                  label: 'int64-type',
+                  t: EnumType([
+                    'native-object',
+                    'big-int',
+                    'big-int-unsigned-only',
+                    'core-int'
+                  ])
+                )
               ])
             )
           ], [
@@ -412,6 +517,57 @@ class DartWitGeneratorWorld {
                   ]),
                   StringType())
             )
+          ]),
+        )!,
+        _generateToFile = library.getComponentFunction(
+          'generate-to-file',
+          const FuncType([
+            (
+              'config',
+              Record([
+                (
+                  label: 'inputs',
+                  t: Union([
+                    Record([(label: 'input-path', t: StringType())]),
+                    Record([
+                      (
+                        label: 'world-file',
+                        t: Record([
+                          (label: 'path', t: StringType()),
+                          (label: 'contents', t: StringType())
+                        ])
+                      ),
+                      (
+                        label: 'pkg-files',
+                        t: ListType(Record([
+                          (label: 'path', t: StringType()),
+                          (label: 'contents', t: StringType())
+                        ]))
+                      )
+                    ])
+                  ])
+                ),
+                (label: 'json-serialization', t: Bool()),
+                (label: 'copy-with', t: Bool()),
+                (label: 'equality-and-hash-code', t: Bool()),
+                (label: 'to-string', t: Bool()),
+                (label: 'generate-docs', t: Bool()),
+                (label: 'file-header', t: OptionType(StringType())),
+                (label: 'use-null-for-option', t: Bool()),
+                (
+                  label: 'int64-type',
+                  t: EnumType([
+                    'native-object',
+                    'big-int',
+                    'big-int-unsigned-only',
+                    'core-int'
+                  ])
+                )
+              ])
+            ),
+            ('file-path', StringType())
+          ], [
+            ('', ResultType(null, StringType()))
           ]),
         )!;
 
@@ -437,6 +593,19 @@ class DartWitGeneratorWorld {
     final results = _generate([config.toJson()]);
     final result = results[0];
     return Result.fromJson(result, (ok) => WitFile.fromJson(ok),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _generateToFile;
+
+  /// Generates a world from the given configuration to the [filePath].
+  Result<void, String> generateToFile({
+    required WitGeneratorConfig config,
+    required String filePath,
+  }) {
+    final results = _generateToFile([config.toJson(), filePath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => null,
         (error) => error is String ? error : (error! as ParsedString).value);
   }
 }
