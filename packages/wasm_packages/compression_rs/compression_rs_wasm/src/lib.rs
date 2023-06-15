dd1c0f701e4223a8a2bf8151a0203571e0531272
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-06-15 00:33:17 -0500

    compression rs test and rust implementation

--- packages/wasm_packages/compression_rs/compression_rs_wasm/src/lib.rs
+++ packages/wasm_packages/compression_rs/compression_rs_wasm/src/lib.rs
@@ -1,9 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 // Use a procedural macro to generate bindings for the world we specified in `compression-rs.wit`
 wit_bindgen::generate!("compression-rs");
 
-// Comment out the following lines to include other generated interfaces
-// use exports::compression_rs_namespace::compression_rs::*;
-// use compression_rs_namespace::compression_rs::interface_name;
+use compression_rs_namespace::compression_rs::flate::Input;
+use exports::compression_rs_namespace::compression_rs;
 
 // Define a custom type and implement the generated trait for it which
 // represents implementing all the necessary exported interfaces for this
@@ -11,6 +13,251 @@ wit_bindgen::generate!("compression-rs");
 struct WitImplementation;
 
 export_compression_rs!(WitImplementation);
+
+impl Input {
+    fn with_read<T>(&mut self, f: impl FnOnce(&mut dyn Read) -> T) -> T {
+        match self {
+            Input::Bytes(bytes) => f(&mut bytes.as_slice()),
+            Input::File(file) => f(&mut File::open(file).unwrap()),
+        }
+    }
+}
+
+fn map_err(e: std::io::Error) -> String {
+    e.to_string()
+}
+
+#[cfg(not(feature = "brotli"))]
+#[allow(unused_variables, unused_mut)]
+impl compression_rs::brotli::Brotli for WitImplementation {
+    fn brotli_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the brotli feature");
+    }
+    fn brotli_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the brotli feature");
+    }
+    fn brotli_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the brotli feature");
+    }
+    fn brotli_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the brotli feature");
+    }
+}
+
+#[cfg(feature = "brotli")]
+impl compression_rs::brotli::Brotli for WitImplementation {
+    fn brotli_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut w = brotli::CompressorWriter::with_params(
+                Vec::new(),
+                4096, /* buffer size */
+                &brotli::enc::BrotliEncoderParams::default(),
+            );
+            std::io::copy(i, &mut w).map_err(map_err)?;
+            Ok(w.into_inner())
+        })
+    }
+
+    fn brotli_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut d = brotli::Decompressor::new(i, 4096 /* buffer size */);
+            let mut output = Vec::new();
+            d.read_to_end(&mut output).map_err(map_err)?;
+            Ok(output)
+        })
+    }
+
+    fn brotli_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let file = File::options().write(true).open(path).map_err(map_err)?;
+            let mut w = brotli::CompressorWriter::with_params(
+                file,
+                4096, /* buffer size */
+                &brotli::enc::BrotliEncoderParams::default(),
+            );
+            let size = std::io::copy(i, &mut w).map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+
+    fn brotli_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let mut d = brotli::Decompressor::new(i, 4096 /* buffer size */);
+            let mut file = File::options().write(true).open(path).map_err(map_err)?;
+            let size = std::io::copy(&mut d, &mut file).map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+}
+
+#[cfg(not(feature = "gzip"))]
+#[allow(unused_variables, unused_mut)]
+impl compression_rs::gzip::Gzip for WitImplementation {
+    fn gzip_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the gzip feature");
+    }
+    fn gzip_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the gzip feature");
+    }
+    fn gzip_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the gzip feature");
+    }
+    fn gzip_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the gzip feature");
+    }
+}
+
+#[cfg(feature = "gzip")]
+impl compression_rs::gzip::Gzip for WitImplementation {
+    fn gzip_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut w = flate2::write::GzEncoder::new(Vec::new(), flate2::Compression::default());
+            std::io::copy(i, &mut w).map_err(map_err)?;
+            Ok(w.finish().map_err(map_err)?)
+        })
+    }
+
+    fn gzip_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::GzDecoder::new(i);
+            let mut output = Vec::new();
+            d.read_to_end(&mut output).map_err(map_err)?;
+            Ok(output)
+        })
+    }
+
+    fn gzip_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let file = File::options().write(true).open(path).map_err(map_err)?;
+            let mut w = flate2::write::GzEncoder::new(file, flate2::Compression::default());
+            let size = std::io::copy(i, &mut w).map_err(map_err)?;
+            w.finish().map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+
+    fn gzip_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::GzDecoder::new(i);
+            let mut file = File::options().write(true).open(path).map_err(map_err)?;
+            let size = std::io::copy(&mut d, &mut file).map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+}
+
+#[cfg(not(feature = "zlib"))]
+#[allow(unused_variables, unused_mut)]
+impl compression_rs::zlib::Zlib for WitImplementation {
+    fn zlib_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the zlib feature");
+    }
+    fn zlib_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the zlib feature");
+    }
+    fn zlib_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the zlib feature");
+    }
+    fn zlib_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the zlib feature");
+    }
+}
+
+#[cfg(feature = "zlib")]
+impl compression_rs::zlib::Zlib for WitImplementation {
+    fn zlib_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut w = flate2::write::ZlibEncoder::new(Vec::new(), flate2::Compression::default());
+            std::io::copy(i, &mut w).map_err(map_err)?;
+            Ok(w.finish().map_err(map_err)?)
+        })
+    }
+
+    fn zlib_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::ZlibDecoder::new(i);
+            let mut output = Vec::new();
+            d.read_to_end(&mut output).map_err(map_err)?;
+            Ok(output)
+        })
+    }
+
+    fn zlib_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let file = File::options().write(true).open(path).map_err(map_err)?;
+            let mut w = flate2::write::ZlibEncoder::new(file, flate2::Compression::default());
+            let size = std::io::copy(i, &mut w).map_err(map_err)?;
+            w.finish().map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+
+    fn zlib_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::ZlibDecoder::new(i);
+            let mut file = File::options().write(true).open(path).map_err(map_err)?;
+            let size = std::io::copy(&mut d, &mut file).map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+}
+
+#[cfg(not(feature = "deflate"))]
+#[allow(unused_variables, unused_mut)]
+impl compression_rs::deflate::Deflate for WitImplementation {
+    fn deflate_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the deflate feature");
+    }
+    fn deflate_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        panic!("The wasm module should be compiled with the deflate feature");
+    }
+    fn deflate_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the deflate feature");
+    }
+    fn deflate_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        panic!("The wasm module should be compiled with the deflate feature");
+    }
+}
+
+#[cfg(feature = "deflate")]
+impl compression_rs::deflate::Deflate for WitImplementation {
+    fn deflate_compress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut w =
+                flate2::write::DeflateEncoder::new(Vec::new(), flate2::Compression::default());
+            std::io::copy(i, &mut w).map_err(map_err)?;
+            Ok(w.finish().map_err(map_err)?)
+        })
+    }
+
+    fn deflate_decompress(mut i: Input) -> Result<Vec<u8>, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::DeflateDecoder::new(i);
+            let mut output = Vec::new();
+            d.read_to_end(&mut output).map_err(map_err)?;
+            Ok(output)
+        })
+    }
+
+    fn deflate_compress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let file = File::options().write(true).open(path).map_err(map_err)?;
+            let mut w = flate2::write::DeflateEncoder::new(file, flate2::Compression::default());
+            let size = std::io::copy(i, &mut w).map_err(map_err)?;
+            w.finish().map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+
+    fn deflate_decompress_file(mut i: Input, path: String) -> Result<u32, String> {
+        i.with_read(|i| {
+            let mut d = flate2::read::DeflateDecoder::new(i);
+            let mut file = File::options().write(true).open(path).map_err(map_err)?;
+            let size = std::io::copy(&mut d, &mut file).map_err(map_err)?;
+            Ok(size as u32)
+        })
+    }
+}
 
 impl CompressionRs for WitImplementation {
     fn run(value: Model) -> Result<f64, String> {
--- packages/wasm_packages/compression_rs/lib/src/compression_rs_wit.gen.dart
+++ packages/wasm_packages/compression_rs/lib/src/compression_rs_wit.gen.dart
@@ -7,6 +7,83 @@ import 'dart:typed_data';
 
 import 'package:wasm_wit_component/wasm_wit_component.dart';
 
+typedef IoSuccess = int /*U32*/;
+typedef IoError = String;
+
+sealed class Input {
+  /// Returns a new instance from a JSON value.
+  /// May throw if the value does not have the expected structure.
+  factory Input.fromJson(Object? json_) {
+    Object? json = json_;
+    if (json is Map) {
+      final k = json.keys.first;
+      json = (
+        k is int ? k : _spec.cases.indexWhere((c) => c.label == k),
+        json.values.first
+      );
+    }
+    return switch (json) {
+      (0, final value) || [0, final value] => InputBytes((value is Uint8List
+          ? value
+          : Uint8List.fromList((value! as List).cast()))),
+      (1, final value) ||
+      [1, final value] =>
+        InputFile(value is String ? value : (value! as ParsedString).value),
+      _ => throw Exception('Invalid JSON $json_'),
+    };
+  }
+  const factory Input.bytes(Uint8List value) = InputBytes;
+  const factory Input.file(String value) = InputFile;
+
+  /// Returns this as a serializable JSON value.
+  Map<String, Object?> toJson();
+
+  /// Returns this as a WASM canonical abi value.
+  (int, Object?) toWasm();
+  static const _spec =
+      Variant([Case('bytes', ListType(U8())), Case('file', StringType())]);
+}
+
+class InputBytes implements Input {
+  final Uint8List value;
+  const InputBytes(this.value);
+
+  /// Returns this as a serializable JSON value.
+  @override
+  Map<String, Object?> toJson() => {'bytes': value.toList()};
+
+  /// Returns this as a WASM canonical abi value.
+  @override
+  (int, Object?) toWasm() => (0, value);
+  @override
+  String toString() => 'InputBytes($value)';
+  @override
+  bool operator ==(Object other) =>
+      other is InputBytes && comparator.areEqual(other.value, value);
+  @override
+  int get hashCode => comparator.hashValue(value);
+}
+
+class InputFile implements Input {
+  final String value;
+  const InputFile(this.value);
+
+  /// Returns this as a serializable JSON value.
+  @override
+  Map<String, Object?> toJson() => {'file': value};
+
+  /// Returns this as a WASM canonical abi value.
+  @override
+  (int, Object?) toWasm() => (1, value);
+  @override
+  String toString() => 'InputFile($value)';
+  @override
+  bool operator ==(Object other) =>
+      other is InputFile && comparator.areEqual(other.value, value);
+  @override
+  int get hashCode => comparator.hashValue(value);
+}
+
 /// A record is a class with named fields
 /// There are enum, list, variant, option, result, tuple and union types
 class Model {
@@ -71,14 +148,314 @@ class CompressionRsWorldImports {
   });
 }
 
+class Brotli {
+  Brotli(WasmLibrary library)
+      : _brotliCompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/brotli#brotli-compress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _brotliDecompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/brotli#brotli-decompress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _brotliCompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/brotli#brotli-compress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!,
+        _brotliDecompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/brotli#brotli-decompress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!;
+  final ListValue Function(ListValue) _brotliCompress;
+  Result<Uint8List, IoError> brotliCompress({
+    required Input input,
+  }) {
+    final results = _brotliCompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _brotliDecompress;
+  Result<Uint8List, IoError> brotliDecompress({
+    required Input input,
+  }) {
+    final results = _brotliDecompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _brotliCompressFile;
+  Result<IoSuccess, IoError> brotliCompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _brotliCompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _brotliDecompressFile;
+  Result<IoSuccess, IoError> brotliDecompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _brotliDecompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+}
+
+class Deflate {
+  Deflate(WasmLibrary library)
+      : _deflateCompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/deflate#deflate-compress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _deflateDecompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/deflate#deflate-decompress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _deflateCompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/deflate#deflate-compress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!,
+        _deflateDecompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/deflate#deflate-decompress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!;
+  final ListValue Function(ListValue) _deflateCompress;
+  Result<Uint8List, IoError> deflateCompress({
+    required Input input,
+  }) {
+    final results = _deflateCompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _deflateDecompress;
+  Result<Uint8List, IoError> deflateDecompress({
+    required Input input,
+  }) {
+    final results = _deflateDecompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _deflateCompressFile;
+  Result<IoSuccess, IoError> deflateCompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _deflateCompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _deflateDecompressFile;
+  Result<IoSuccess, IoError> deflateDecompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _deflateDecompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+}
+
+class Gzip {
+  Gzip(WasmLibrary library)
+      : _gzipCompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/gzip#gzip-compress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _gzipDecompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/gzip#gzip-decompress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _gzipCompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/gzip#gzip-compress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!,
+        _gzipDecompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/gzip#gzip-decompress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!;
+  final ListValue Function(ListValue) _gzipCompress;
+  Result<Uint8List, IoError> gzipCompress({
+    required Input input,
+  }) {
+    final results = _gzipCompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _gzipDecompress;
+  Result<Uint8List, IoError> gzipDecompress({
+    required Input input,
+  }) {
+    final results = _gzipDecompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _gzipCompressFile;
+  Result<IoSuccess, IoError> gzipCompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _gzipCompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _gzipDecompressFile;
+  Result<IoSuccess, IoError> gzipDecompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _gzipDecompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+}
+
+class Zlib {
+  Zlib(WasmLibrary library)
+      : _zlibCompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/zlib#zlib-compress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _zlibDecompress = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/zlib#zlib-decompress',
+          const FuncType([('input', Input._spec)],
+              [('', ResultType(ListType(U8()), StringType()))]),
+        )!,
+        _zlibCompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/zlib#zlib-compress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!,
+        _zlibDecompressFile = library.getComponentFunction(
+          'compression-rs-namespace:compression-rs/zlib#zlib-decompress-file',
+          const FuncType(
+              [('input', Input._spec), ('output-path', StringType())],
+              [('', ResultType(U32(), StringType()))]),
+        )!;
+  final ListValue Function(ListValue) _zlibCompress;
+  Result<Uint8List, IoError> zlibCompress({
+    required Input input,
+  }) {
+    final results = _zlibCompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _zlibDecompress;
+  Result<Uint8List, IoError> zlibDecompress({
+    required Input input,
+  }) {
+    final results = _zlibDecompress([input.toWasm()]);
+    final result = results[0];
+    return Result.fromJson(
+        result,
+        (ok) =>
+            (ok is Uint8List ? ok : Uint8List.fromList((ok! as List).cast())),
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _zlibCompressFile;
+  Result<IoSuccess, IoError> zlibCompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _zlibCompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+
+  final ListValue Function(ListValue) _zlibDecompressFile;
+  Result<IoSuccess, IoError> zlibDecompressFile({
+    required Input input,
+    required String outputPath,
+  }) {
+    final results = _zlibDecompressFile([input.toWasm(), outputPath]);
+    final result = results[0];
+    return Result.fromJson(result, (ok) => ok! as int,
+        (error) => error is String ? error : (error! as ParsedString).value);
+  }
+}
+
 class CompressionRsWorld {
   final CompressionRsWorldImports imports;
   final WasmLibrary library;
+  final Brotli brotli;
+  final Deflate deflate;
+  final Gzip gzip;
+  final Zlib zlib;
 
   CompressionRsWorld({
     required this.imports,
     required this.library,
-  }) : _run = library.getComponentFunction(
+  })  : brotli = Brotli(library),
+        deflate = Deflate(library),
+        gzip = Gzip(library),
+        zlib = Zlib(library),
+        _run = library.getComponentFunction(
           'run',
           const FuncType([('value', Model._spec)],
               [('', ResultType(Float64(), StringType()))]),
