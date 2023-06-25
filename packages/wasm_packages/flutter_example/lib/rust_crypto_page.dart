356c25a17873820bf121fd34bb821ae26462de9e
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-06-24 19:53:29 -0500

    rust crypto page implementation

--- packages/wasm_packages/flutter_example/lib/rust_crypto_page.dart
+++ packages/wasm_packages/flutter_example/lib/rust_crypto_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_example/flutter_utils.dart';
+import 'package:flutter_example/rust_crypto_state.dart';
 import 'package:flutter_example/state.dart';
 
 class RustCryptoPage extends StatelessWidget {
@@ -12,9 +14,340 @@ class RustCryptoPage extends StatelessWidget {
     if (state == null) {
       return const CircularProgressIndicator();
     }
+    final isSmall = MediaQuery.of(context).size.width < 850;
+    return AnimatedBuilder(
+      animation: state,
+      builder: (context, _) {
+        if (isSmall) {
+          return DefaultTabController(
+            length: 2,
+            child: Column(
+              children: [
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      SingleChildScrollView(
+                        child: HashAndHmacView(state: state),
+                      ),
+                      SingleChildScrollView(child: Aes256GcmView(state: state)),
+                    ],
+                  ),
+                ),
+                const TabBar(
+                  tabs: [
+                    Tab(child: Text('HASH & MAC')),
+                    Tab(child: Text('AES GCM SIV')),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SingleChildScrollView(
+              child: HashAndHmacView(state: state),
+            ),
+            const SizedBox(width: 20),
+            SingleChildScrollView(
+              child: Aes256GcmView(state: state),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class HashAndHmacView extends StatelessWidget {
+  const HashAndHmacView({
+    super.key,
+    required this.state,
+  });
+
+  final RustCryptoState state;
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        const Placeholder(),
+        const Text('HASH and HMAC').title(),
+        const SizedBox(height: 8),
+        BinaryInputWidget(data: state.hashInput, label: 'Hash Input'),
+        BinaryInputWidget(
+          data: state.hmacKeyInput,
+          label: 'Hmac Key (512bit/64Byte)',
+        ),
+        const SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            BinaryDataEncodingButtons(
+              data: BinaryInputData(
+                () => state.hashOutputEncodingSet(
+                  state.hashOutputEncoding == InputEncoding.hex
+                      ? InputEncoding.base64
+                      : InputEncoding.hex,
+                ),
+                isKey: true,
+              )..encoding = state.hashOutputEncoding,
+            ),
+            ElevatedButton(
+              onPressed: state.computeHashValues,
+              child: const Text('Compute'),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            const Text('Algorithm').container(width: 70),
+            const Expanded(child: Text('Hash', textAlign: TextAlign.center)),
+            const Expanded(child: Text('Hmac', textAlign: TextAlign.center)),
+          ],
+        ),
+        Column(
+          children: [
+            ...RCHash.values.map(
+              (e) => Row(
+                children: [
+                  SelectableText(e.name).container(width: 70),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SelectableText(
+                            state.hashValues[e] ?? '',
+                            style: const TextStyle(
+                              overflow: TextOverflow.clip,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                        copyButton(state.hashValues[e] ?? ''),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SelectableText(
+                            state.hmacValues[e] ?? '',
+                            style: const TextStyle(
+                              overflow: TextOverflow.fade,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                        copyButton(state.hashValues[e] ?? ''),
+                      ],
+                    ),
+                  ),
+                ],
+              ).container(
+                padding: const EdgeInsets.all(4),
+                height: 60,
+                // width: 300,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ).container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      constraints: const BoxConstraints(maxWidth: 400),
+    );
+  }
+}
+
+class Aes256GcmView extends StatelessWidget {
+  const Aes256GcmView({
+    super.key,
+    required this.state,
+  });
+
+  final RustCryptoState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('AES 256 GCM SIV').title(),
+        const SizedBox(height: 8),
+        BinaryInputWidget(
+          data: state.aesKeyInput,
+          label: 'Aes Key (256bit/32Byte)',
+        ),
+        TextField(
+          controller: state.nonceController,
+          decoration: InputDecoration(
+            labelText: 'Nonce (base64 - 96bit/12Byte)',
+            suffixIcon: IconButton(
+              onPressed: state.generateNonce,
+              icon: const Icon(Icons.refresh),
+            ),
+          ),
+        ),
+        TextField(
+          controller: state.associatedDataController,
+          decoration: const InputDecoration(
+            labelText: 'Associated Data (UTF-8)',
+          ),
+        ),
+        BinaryInputWidget(
+          data: state.planTextInput,
+          label: 'Plain Text',
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Nonce Concatenation'),
+            const SizedBox(width: 4),
+            Checkbox(
+              onChanged: (_) => state.isConcatToggle(),
+              value: state.isConcat,
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: state.encrypt,
+              icon: const Icon(Icons.arrow_downward_rounded),
+              label: const Text('Encrypt'),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton.icon(
+              onPressed: state.decrypt,
+              icon: const Icon(Icons.arrow_upward_rounded),
+              label: const Text('Decrypt'),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        BinaryInputWidget(
+          data: state.cipherTextInput,
+          label: 'Cipher Text',
+        ),
+        // Column(
+        //   children: [
+        //     const Text('base64CipherTextOutput'),
+        //     SelectableText(state.base64CipherOutput),
+        //   ],
+        // ),
+      ],
+    ).container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      constraints: const BoxConstraints(maxWidth: 400),
+    );
+  }
+}
+
+class BinaryInputWidget extends StatelessWidget {
+  const BinaryInputWidget({
+    super.key,
+    required this.data,
+    required this.label,
+  });
+
+  final BinaryInputData data;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    final file = data.file;
+    return Column(
+      children: [
+        BinaryDataEncodingButtons(data: data),
+        if (data.encoding == InputEncoding.file)
+          Card(
+            child: Column(
+              children: [
+                SizedBox(height: 54, child: Text(file!.name).subtitle()),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () => downloadFile(file.name, file.bytes),
+                      icon: const Icon(Icons.download),
+                      label: const Text('Download'),
+                    ),
+                    const SizedBox(width: 6),
+                    Text(file.bytes.sizeHuman),
+                    const SizedBox(width: 6),
+                    TextButton(
+                      onPressed: () => data.encodingSet(InputEncoding.file),
+                      child: const Text('Pick File'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 6),
+              ],
+            ),
+          ).container(constraints: const BoxConstraints(maxWidth: 500))
+        else
+          TextField(
+            controller: data.textController,
+            maxLines: data.isKey ? 3 : 4,
+            style: data.isKey
+                ? const TextStyle(fontSize: 12)
+                : const TextStyle(fontSize: 12),
+            decoration: InputDecoration(
+              labelText: label,
+              suffixIcon: data.generate != null
+                  ? Column(
+                      children: [
+                        IconButton(
+                          onPressed: data.generate,
+                          icon: const Icon(Icons.refresh),
+                        ),
+                        copyButton(data.textController.text),
+                      ],
+                    )
+                  : copyButton(data.textController.text),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+Widget copyButton(String text) {
+  return IconButton(
+    iconSize: 16,
+    onPressed: () => Clipboard.setData(
+      ClipboardData(text: text),
+    ),
+    icon: const Icon(Icons.copy),
+  );
+}
+
+class BinaryDataEncodingButtons extends StatelessWidget {
+  const BinaryDataEncodingButtons({super.key, required this.data});
+
+  final BinaryInputData data;
+
+  @override
+  Widget build(BuildContext context) {
+    final allowedEncodings = data.allowedEncodings;
+    return ToggleButtons(
+      constraints: const BoxConstraints(minHeight: 35),
+      isSelected: allowedEncodings.map((e) => e == data.encoding).toList(),
+      onPressed: (index) {
+        data.encodingSet(allowedEncodings[index]);
+      },
+      children: [
+        ...allowedEncodings.map(
+          (e) => Text(e.name).container(alignment: Alignment.center, width: 60),
+        ),
       ],
     );
   }
--- packages/wasm_packages/flutter_example/lib/rust_crypto_state.dart
+++ packages/wasm_packages/flutter_example/lib/rust_crypto_state.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
 
-import 'package:flutter/foundation.dart' show ChangeNotifier;
+import 'package:file_system_access/file_system_access.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_example/flutter_utils.dart';
 import 'package:rust_crypto/rust_crypto.dart';
 
@@ -13,12 +15,116 @@ enum RCHash {
   blake3,
 }
 
+class BinaryInputData {
+  BinaryInputData(
+    this._notifyListeners, {
+    required this.isKey,
+    this.generate,
+    List<InputEncoding>? allowedEncodings,
+  })  : encoding = allowedEncodings?.first ??
+            (isKey ? InputEncoding.base64 : InputEncoding.utf8),
+        allowedEncodings = allowedEncodings ??
+            (isKey
+                ? [InputEncoding.base64, InputEncoding.hex]
+                : InputEncoding.values);
+
+  final void Function()? generate;
+  final void Function() _notifyListeners;
+  final List<InputEncoding> allowedEncodings;
+  final bool isKey;
+
+  final textController = TextEditingController();
+
+  String get text => textController.text;
+
+  CryptoFileInput? file;
+  InputEncoding encoding;
+
+  Uint8List get bytes => inputBytes(encoding, text, file);
+
+  void encodingSet(InputEncoding encoding) async {
+    if (this.encoding == encoding) return;
+    if (encoding == InputEncoding.file) {
+      if (file == null) {
+        final files = await FileSystem.instance
+            .showOpenFilePickerWebSafe(const FsOpenOptions(multiple: false));
+        if (files.isEmpty) return;
+        final file_ = files.first.file;
+        final bytes = await file_.readAsBytes();
+        file = CryptoFileInput(file_.name, bytes);
+      }
+    } else {
+      if (this.encoding == InputEncoding.file && file!.bytes.length > 1000000) {
+        this.encoding = InputEncoding.fromText(text);
+      }
+      textController.text =
+          outputText(encoding, inputBytes(this.encoding, text, file));
+    }
+    this.encoding = encoding;
+    _notifyListeners();
+  }
+}
+
 class RustCryptoState extends ChangeNotifier with ErrorNotifier {
-  RustCryptoState(this.rustCrypto);
+  RustCryptoState(this.rustCrypto) {
+    generateAesKey();
+    generateHmacKey();
+    generateNonce();
+  }
+
+  void generateNonce() {
+    nonceController.text = outputText(
+      InputEncoding.base64,
+      rustCrypto.aesGcmSiv.generateNonce(),
+    );
+  }
+
+  void generateHmacKey() {
+    hmacKeyInput.textController.text = outputText(
+      hmacKeyInput.encoding,
+      generateRandomBytes(64),
+    );
+  }
+
+  void generateAesKey() {
+    aesKeyInput.textController.text = outputText(
+      InputEncoding.base64,
+      rustCrypto.aesGcmSiv.generateKey(kind: AesKind.bits256),
+    );
+  }
 
   final RustCryptoWorld rustCrypto;
+  final nonceController = TextEditingController();
+  final associatedDataController = TextEditingController();
 
-  Uint8List hashBytes(Uint8List bytes, RCHash type) {
+  late final hashInput = BinaryInputData(notifyListeners, isKey: false);
+  late final hmacKeyInput =
+      BinaryInputData(notifyListeners, isKey: true, generate: generateHmacKey);
+  late final aesKeyInput =
+      BinaryInputData(notifyListeners, isKey: true, generate: generateAesKey);
+  late final planTextInput = BinaryInputData(notifyListeners, isKey: false);
+  late final cipherTextInput = BinaryInputData(
+    notifyListeners,
+    isKey: false,
+    allowedEncodings: const [
+      InputEncoding.base64,
+      InputEncoding.hex,
+      InputEncoding.file,
+    ],
+  );
+
+  InputEncoding hashOutputEncoding = InputEncoding.base64;
+  // String base64CipherOutput = '';
+
+  void hashOutputEncodingSet(InputEncoding encoding) {
+    hashOutputEncoding = encoding;
+    computeHashValues();
+  }
+
+  Map<RCHash, String> hmacValues = {};
+  Map<RCHash, String> hashValues = {};
+
+  Uint8List _hashBytes(Uint8List bytes, RCHash type) {
     final hash = switch (type) {
       RCHash.blake3 => rustCrypto.blake3.hash(bytes: bytes),
       RCHash.sha224 => rustCrypto.sha2.sha224(bytes: bytes),
@@ -26,18 +132,118 @@ class RustCryptoState extends ChangeNotifier with ErrorNotifier {
       RCHash.sha384 => rustCrypto.sha2.sha384(bytes: bytes),
       RCHash.sha512 => rustCrypto.sha2.sha512(bytes: bytes),
     };
+    hashValues[type] = outputText(hashOutputEncoding, hash);
     return hash;
   }
 
-  Uint8List hmacBytes(Uint8List bytes, Uint8List key, RCHash type) {
+  Uint8List _hmacBytes(Uint8List bytes, Uint8List key, RCHash type) {
     final hmac = switch (type) {
-      RCHash.blake3 => rustCrypto.blake3.macKeyedHash(bytes: bytes, key: key),
+      RCHash.blake3 =>
+        rustCrypto.blake3.macKeyedHash(bytes: bytes, key: key.sublist(0, 32)),
       RCHash.sha224 => rustCrypto.hmac.hmacSha224(bytes: bytes, key: key),
       RCHash.sha256 => rustCrypto.hmac.hmacSha256(bytes: bytes, key: key),
       RCHash.sha384 => rustCrypto.hmac.hmacSha384(bytes: bytes, key: key),
       RCHash.sha512 => rustCrypto.hmac.hmacSha512(bytes: bytes, key: key),
     };
+    hmacValues[type] = outputText(hashOutputEncoding, hmac);
     return hmac;
+  }
+
+  void computeHashValues() {
+    final bytes = hashInput.bytes;
+    final key = hmacKeyInput.text.isNotEmpty ? hmacKeyInput.bytes : null;
+    for (final type in RCHash.values) {
+      _hashBytes(bytes, type);
+      if (key != null) {
+        _hmacBytes(bytes, key, type);
+      }
+    }
+    if (key == null) hmacValues.clear();
+    notifyListeners();
+  }
+
+  bool isConcat = false;
+
+  void isConcatToggle() {
+    isConcat = !isConcat;
+    notifyListeners();
+  }
+
+  static const encryptedExt = '.aes256gcmsiv';
+
+  void encrypt() {
+    final plainText = planTextInput.bytes;
+    final nonce_ = inputBytes(InputEncoding.base64, nonceController.text, null);
+    final associatedData_ =
+        inputBytes(InputEncoding.utf8, associatedDataController.text, null);
+
+    final Uint8List encrypted;
+    if (isConcat) {
+      encrypted = rustCrypto.aesGcmSiv.encryptConcat(
+        AesKind.bits256,
+        plainText: plainText,
+        key: aesKeyInput.bytes,
+        nonce: nonce_,
+        associatedData: associatedData_,
+      );
+    } else {
+      encrypted = rustCrypto.aesGcmSiv.encrypt(
+        kind: AesKind.bits256,
+        plainText: plainText,
+        key: aesKeyInput.bytes,
+        nonce: nonce_,
+        associatedData: associatedData_,
+      );
+    }
+    if (planTextInput.encoding == InputEncoding.file) {
+      final file = planTextInput.file!;
+      cipherTextInput.file =
+          CryptoFileInput('${file.name}$encryptedExt', encrypted);
+      cipherTextInput.encoding = InputEncoding.file;
+    } else {
+      cipherTextInput.textController.text =
+          outputText(cipherTextInput.encoding, encrypted);
+    }
+    notifyListeners();
+  }
+
+  void decrypt() {
+    final cipherText = cipherTextInput.bytes;
+    final Uint8List decrypted;
+    if (isConcat) {
+      decrypted = rustCrypto.aesGcmSiv.decryptConcat(
+        AesKind.bits256,
+        concat: cipherText,
+        key: aesKeyInput.bytes,
+      );
+    } else {
+      decrypted = rustCrypto.aesGcmSiv.decrypt(
+        kind: AesKind.bits256,
+        cipherText: cipherText,
+        key: aesKeyInput.bytes,
+        nonce: inputBytes(InputEncoding.base64, nonceController.text, null),
+        associatedData:
+            inputBytes(InputEncoding.utf8, associatedDataController.text, null),
+      );
+    }
+    if (cipherTextInput.encoding == InputEncoding.file) {
+      final file = cipherTextInput.file!;
+      final name = file.name.endsWith(encryptedExt)
+          ? file.name.substring(0, file.name.length - encryptedExt.length)
+          : file.name;
+      planTextInput.file = CryptoFileInput(name, decrypted);
+      planTextInput.encoding = InputEncoding.file;
+    } else {
+      try {
+        planTextInput.textController.text =
+            outputText(planTextInput.encoding, decrypted);
+      } catch (_) {
+        planTextInput.encoding = InputEncoding.base64;
+        planTextInput.textController.text =
+            outputText(InputEncoding.base64, decrypted);
+      }
+    }
+    notifyListeners();
   }
 }
 
@@ -48,6 +254,7 @@ extension AesGcmSivExt on AesGcmSiv {
     required Uint8List key,
     Uint8List? nonce,
     Uint8List? associatedData,
+    bool concatAssociatedData = false,
   }) {
     final n = nonce ?? generateNonce();
     final associated = associatedData ?? Uint8List(0);
@@ -58,11 +265,13 @@ extension AesGcmSivExt on AesGcmSiv {
       plainText: plainText,
       associatedData: associated,
     );
-    final data = ByteData(associated.length + n.length + cipherText.length);
+    final nonceStart = concatAssociatedData ? 4 + associated.length : 0;
+    final data = ByteData(nonceStart + n.length + cipherText.length);
     final view = data.buffer.asUint8List();
-    data.setUint32(0, associated.length);
-    view.setAll(4, associated);
-    final nonceStart = 4 + associated.length;
+    if (concatAssociatedData) {
+      data.setUint32(0, associated.length);
+      view.setAll(4, associated);
+    }
     view.setAll(nonceStart, n);
     view.setAll(nonceStart + n.length, cipherText);
     return view;
@@ -72,11 +281,17 @@ extension AesGcmSivExt on AesGcmSiv {
     AesKind kind, {
     required Uint8List concat,
     required Uint8List key,
+    Uint8List? associatedData,
+    bool concatAssociatedData = false,
   }) {
     final data = ByteData.sublistView(concat);
-    final ascLen = data.getUint32(0);
-    final nonceStart = ascLen + 4;
-    final associatedData = Uint8List.sublistView(concat, 4, nonceStart);
+    int nonceStart = 0;
+    Uint8List? associatedData_ = associatedData;
+    if (concatAssociatedData) {
+      final ascLen = data.getUint32(0);
+      nonceStart = ascLen + 4;
+      associatedData_ ??= Uint8List.sublistView(concat, 4, nonceStart);
+    }
     final nonce = Uint8List.sublistView(concat, nonceStart, nonceStart + 12);
     final cipherText = Uint8List.sublistView(concat, nonceStart + 12);
     return decrypt(
@@ -84,12 +299,96 @@ extension AesGcmSivExt on AesGcmSiv {
       key: key,
       nonce: nonce,
       cipherText: cipherText,
-      associatedData: associatedData,
+      associatedData: associatedData_,
     );
   }
 
   Uint8List generateNonce([Random? random]) {
-    final r = random ?? Random.secure();
-    return Uint8List.fromList(List.generate(12, (_) => r.nextInt(256)));
+    return generateRandomBytes(12, random);
   }
 }
+
+Uint8List generateRandomBytes(int bytes, [Random? random]) {
+  final r = random ?? Random.secure();
+  return Uint8List.fromList(List.generate(bytes, (_) => r.nextInt(256)));
+}
+
+class CryptoFileInput {
+  CryptoFileInput(this.name, this.bytes);
+
+  final String name;
+  final Uint8List bytes;
+}
+
+enum InputEncoding {
+  utf8,
+  hex,
+  base64,
+  file;
+
+  factory InputEncoding.fromText(String text) {
+    try {
+      hexToBytes(text);
+      return InputEncoding.hex;
+    } catch (_) {}
+    try {
+      inputBytes(InputEncoding.base64, text, null);
+      return InputEncoding.base64;
+    } catch (_) {}
+    return InputEncoding.utf8;
+  }
+}
+
+const _HEX_ALPHABET = '0123456789abcdef';
+
+Uint8List hexToBytes(String hex) {
+  String str = hex.replaceAll(' ', '');
+  str = str.toLowerCase();
+  if (str.length % 2 != 0) {
+    str = '0$str';
+  }
+  final result = Uint8List(str.length ~/ 2);
+  for (int i = 0; i < result.length; i++) {
+    int firstDigit = _HEX_ALPHABET.indexOf(str[i * 2]);
+    int secondDigit = _HEX_ALPHABET.indexOf(str[i * 2 + 1]);
+    if (firstDigit == -1 || secondDigit == -1) {
+      throw FormatException('Non-hex character detected in $hex');
+    }
+    result[i] = (firstDigit << 4) + secondDigit;
+  }
+  return result;
+}
+
+String bytesToHex(List<int> bytes) {
+  final buffer = StringBuffer();
+  for (int part in bytes) {
+    if (part & 0xff != part) {
+      throw const FormatException('Non-byte integer detected');
+    }
+    buffer.write('${part < 16 ? '0' : ''}${part.toRadixString(16)}');
+  }
+  return buffer.toString();
+}
+
+Uint8List inputBytes(
+  InputEncoding inputEncoding,
+  String text,
+  CryptoFileInput? file,
+) =>
+    switch (inputEncoding) {
+      InputEncoding.utf8 => const Utf8Encoder().convert(text),
+      InputEncoding.hex => hexToBytes(text),
+      InputEncoding.base64 => base64.decode(text),
+      InputEncoding.file => file!.bytes,
+    };
+
+String outputText(
+  InputEncoding outputEncoding,
+  Uint8List bytes,
+) =>
+    switch (outputEncoding) {
+      InputEncoding.utf8 => const Utf8Decoder().convert(bytes),
+      InputEncoding.hex => bytesToHex(bytes),
+      InputEncoding.base64 => base64.encode(bytes),
+      InputEncoding.file => throw UnimplementedError(),
+    };
