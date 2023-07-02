467a40ebcb10e7352b96e71bcf8b50f35badf429
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-07-02 08:57:18 -0500

    fix sqlite3.wasm path and run test on every push

--- packages/wasm_packages/flutter_example/lib/sqlite/_sqlite_web.dart
+++ packages/wasm_packages/flutter_example/lib/sqlite/_sqlite_web.dart
@@ -2,8 +2,8 @@ import 'package:sqlite3/wasm.dart';
 
 Future<CommonSqlite3> loadSqlite() async {
   // TODO: use SimpleOpfsFileSystem
-  final fs = await IndexedDbFileSystem.open(dbName: 'my_app');
-  final wasm = await WasmSqlite3.loadFromUrl(Uri.parse('sqlite.wasm'));
+  final fs = await IndexedDbFileSystem.open(dbName: 'sql_parser_wasm');
+  final wasm = await WasmSqlite3.loadFromUrl(Uri.parse('sqlite3.wasm'));
   wasm.registerVirtualFileSystem(fs);
   return wasm;
 }
