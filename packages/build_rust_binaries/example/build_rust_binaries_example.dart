e2b951fff8889be5c045187baf6a22b4248a1c82
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2026-04-25 15:34:32 -0500

    base build_rust_binaries package

---
+++ packages/build_rust_binaries/example/build_rust_binaries_example.dart
@@ -0,0 +1,6 @@
+import 'package:build_rust_binaries/build_rust_binaries.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/build_rust_binaries/test/build_rust_binaries_test.dart
@@ -0,0 +1,16 @@
+import 'package:build_rust_binaries/build_rust_binaries.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
